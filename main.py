deefdb74857c89d7a835d3ba8d51352214ae80e2
Andres Bonilla <andresdb91@gmail.com>
2020-11-15 22:24:33 -0300

    Fix import routes and make token parameter not required

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import threading
 from config.default import config
 import instance.config
 
-from game import Game
+from game.client import GameClient
 from ui import UI
 
 
 if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('token', help='Override token in configuration', type=str)
+    parser.add_argument('-t', '--token', help='Override token in configuration', type=str)
 
     cli = parser.add_mutually_exclusive_group()
     cli.add_argument('-i', '--interface', action='store_true', help='Enable command line interface')
@@ -25,17 +25,22 @@ if __name__ == '__main__':
 
     # Override stored configuration with parameters
     if cmd_args.token:
-        print("Using token from command line")
+        print('Using token from command line')
         config['auth_token'] = cmd_args.token
     if cmd_args.interface:
+        print('Command line interface enabled')
         config['interface'] = True
+
+    # Create game instance
+    game = GameClient(config)
 
     # Start cli interface
     if config.get('interface'):
-        ui = UI()
+        ui = UI(game)
+        print('Starting UI thread')
         T = threading.Thread(target=ui.cli, daemon=True)
         T.start()
 
     # Call event loop
-    game = Game(config)
+    print('Starting game loop')
     asyncio.run(game.main())
