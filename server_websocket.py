40c5f505f3aa04bf6bb02b654993e6a949689d5a
Andres Bonilla <andresdb91@gmail.com>
2020-11-15 22:25:16 -0300

    Implement basic websocket server adapter

---
+++ server_websocket.py
@@ -0,0 +1,33 @@
+import websockets
+import json
+
+
+class ServerWebsocketAdap:
+
+    websocket: websockets.WebSocketClientProtocol
+
+    def __init__(self, uri):
+        self.uri = uri
+
+    # TODO: Implement own context manager
+    async def exec_with_context(self, callback, condition):
+        while condition:
+            try:
+                async with websockets.connect(self.uri) as self.websocket:
+                    print('Connected to websocket server')
+                    await callback()
+            except Exception as e:
+                print(f'Error: {e}')
+
+    async def recv(self):
+        response = await self.websocket.recv()
+        print(f'Recv: {response}')
+        return json.loads(response)
+
+    async def send(self, action, data):
+        msg = json.dumps({
+            'action': action,
+            'data': data,
+        })
+        print(f'Sent: {msg}')
+        await self.websocket.send(msg)
