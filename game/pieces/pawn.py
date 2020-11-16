304ecf09b8730535dbb764ff92bb56527ba69857
Andres Bonilla <andresdb91@gmail.com>
2020-11-15 22:32:03 -0300

    Add pawn piece class and moves calculation

---
+++ game/pieces/pawn.py
@@ -0,0 +1,27 @@
+from game.pieces.piece import Piece
+
+
+class Pawn(Piece):
+    POINTS = 10
+
+    def __init__(self, color, x, y):
+        super(Pawn, self).__init__(color, x, y, Pawn.POINTS)
+
+    def update_moves(self):
+        if self.color == 'white':
+            y0 = [2, 3]
+            direction = 1
+        else:
+            y0 = [12, 13]
+            direction = -1
+
+        self.moves.append((
+            (self.x, self.y),
+            (self.x, self.y + direction)
+        ))
+
+        if self.y in y0:
+            self.moves.append((
+                (self.x, self.y),
+                (self.x, self.y + 2*direction)
+            ))
