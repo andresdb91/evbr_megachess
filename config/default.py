3027530bb72e08a6a85629d4a884954f4fc899d6
Andres Bonilla <andresdb91@gmail.com>
2020-11-15 22:20:11 -0300

    Add more default configuration options

--- config/default.py
+++ config/default.py
@@ -1,4 +1,7 @@
 config = {
     'websocket_uri': 'ws://megachess.herokuapp.com/service?authtoken={}',
     'ai_strategy': 'random',
+    'interface': True,
+    'accept_challenges': True,
+    'max_games': 5,
 }
