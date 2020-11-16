f64ebb910c4846908421fe5bb1d7d252250ef4d7
Andres Bonilla <andresdb91@gmail.com>
2020-11-15 23:45:32 -0300

    Add board object

--- game/instance.py
+++ game/instance.py
@@ -1,5 +1,6 @@
 from game.strategies.base_strat import BaseStrategy
 from game.strategies.strategies_fact import AIStrategyFactory
+from game.board import Board
 
 
 class GameInstance:
@@ -10,16 +11,20 @@ class GameInstance:
     move_left: str
     color: str
     strategy: BaseStrategy
+    board: Board
 
-    def __init__(self, config, board_id, opponent, move_left, color):
+    def __init__(self, config, board_id, opponent, move_left, color, board):
         self.config = config.copy()
         self.board_id = board_id
         self.opponent = opponent
         self.move_left = move_left
         self.color = color
         self.strategy = AIStrategyFactory.get_strategy(self.config.get('ai_strategy', 'random'))
+        self.board = Board(board)
 
-    async def play(self, turn_token, board, server):
+    async def play(self, turn_token, server, board=None):
+        if board:
+            self.board.update(board)
         y1, x1, y2, x2 = self.strategy.play(board)
         await server.send(
             'move',
