2869dd0789ac4aa87dfc3ada25eef2672a5b81a5
Andres Bonilla <andresdb91@gmail.com>
2020-11-16 05:05:19 -0300

    Change default strategy to random legal

--- config/default.py
+++ config/default.py
@@ -1,6 +1,6 @@
 config = {
     'websocket_uri': 'ws://megachess.herokuapp.com/service?authtoken={}',
-    'ai_strategy': 'random',
+    'ai_strategy': 'random_legal',
     'interface': True,
     'accept_challenges': True,
     'max_games': 5,
