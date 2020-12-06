1d2aa52fca4862c8e098b65f9853cb425d139641
Andres Bonilla <andresdb91@gmail.com>
2020-12-05 23:30:07 -0300

    Add test for undoing moves

--- test/test_move.py
+++ test/test_move.py
@@ -48,3 +48,21 @@ class TestMove(unittest.TestCase):
             self.assertEqual(to_piece_post_move, pieces.Queen)
         else:
             self.assertEqual(from_piece_pre_move, to_piece_post_move)
+
+    @parameterized.expand([
+        ((2, 2, 2, 3, pieces.Pawn, 10, 'black'),),
+        ((2, 13, 2, 12, pieces.Pawn, 10, 'white'),),
+        ((7, 6, 7, 7, pieces.Pawn, 510, 'black'),),
+        ((7, 9, 7, 8, pieces.Pawn, 510, 'white'),),
+    ])
+    def test_undo(self, move_data):
+        move = Move(*move_data)
+        from_piece_pre_move = self.test_board.get_piece(move.from_x, move.from_y)
+        to_piece_pre_move = self.test_board.get_piece(move.to_x, move.to_y)
+        move.execute(self.test_board)
+        move.undo(self.test_board)
+        from_piece_post_move = self.test_board.get_piece(move.from_x, move.from_y)
+        to_piece_post_move = self.test_board.get_piece(move.to_x, move.to_y)
+
+        self.assertEqual(from_piece_pre_move, from_piece_post_move)
+        self.assertEqual(to_piece_pre_move, to_piece_post_move)
