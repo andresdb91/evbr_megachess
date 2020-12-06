1a1041764666000bd05e604ba8d4e4f177a27472
Andres Bonilla <andresdb91@gmail.com>
2020-12-06 01:44:57 -0300

    Move dict to class (out of function)

--- game/strategies_fact.py
+++ game/strategies_fact.py
@@ -4,19 +4,19 @@ import game.strategies
 class AIStrategyFactory:
     default_strategy: str = 'random_legal'
 
+    strategies = {
+        'random_legal': game.strategies.RandomLegal,
+        'maximum_points': game.strategies.MaximumPointMove,
+        'maximum_weight': game.strategies.MaximumWeight,
+        'pawns_and_queens': game.strategies.OnlyPawnsAndQueensByWeight,
+        '2_move_weight': game.strategies.TwoMoveWeighting,
+        'multi_move_weight': game.strategies.MultiMoveWeight,
+    }
+
     @staticmethod
     def get_strategy(name: str) -> game.strategies.BaseStrategy:
-        strategies = {
-            'random_legal': game.strategies.RandomLegal,
-            'maximum_points': game.strategies.MaximumPointMove,
-            'maximum_weight': game.strategies.MaximumWeight,
-            'pawns_and_queens': game.strategies.OnlyPawnsAndQueensByWeight,
-            '2_move_weight': game.strategies.TwoMoveWeighting,
-            'multi_move_weight': game.strategies.MultiMoveWeight,
-        }
-
-        if name not in strategies.keys():
+        if name not in AIStrategyFactory.strategies.keys():
             name = AIStrategyFactory.default_strategy
         print(f'Using strategy: {name}')
 
-        return strategies[name]()
+        return AIStrategyFactory.strategies[name]()
--- server/server_fact.py
+++ server/server_fact.py
@@ -4,14 +4,14 @@ from server import server_adap
 class ServerAdapterFactory:
     default_adapter: str = 'websocket'
 
+    adapters = {
+        'websocket': server_adap.ServerWebsocketAdap,
+    }
+
     @staticmethod
     def get_adapter(name: str, *args) -> server_adap.BaseServerAdapter:
-        adapters = {
-            'websocket': server_adap.ServerWebsocketAdap,
-        }
-
-        if name not in adapters.keys():
+        if name not in ServerAdapterFactory.adapters.keys():
             name = ServerAdapterFactory.default_adapter
         print(f'Using server adapter: {name}')
 
-        return adapters[name](*args)
+        return ServerAdapterFactory.adapters[name](*args)
