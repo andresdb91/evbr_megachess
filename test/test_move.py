75fce9cd85c332751a457456afde9ff9d91b1b74
Andres Bonilla <andresdb91@gmail.com>
2020-12-05 23:27:28 -0300

    Add test for executing moves

--- test/test_move.py
+++ test/test_move.py
@@ -28,3 +28,23 @@ class TestMove(unittest.TestCase):
 
     def setUp(self) -> None:
         self.test_board = board.Board(self.test_board_str)
+
+    @parameterized.expand([
+        ((2, 2, 2, 3, pieces.Pawn, 10, 'black'), False),
+        ((2, 13, 2, 12, pieces.Pawn, 10, 'white'), False),
+        ((7, 6, 7, 7, pieces.Pawn, 510, 'black'), True),
+        ((7, 9, 7, 8, pieces.Pawn, 510, 'white'), True),
+    ])
+    def test_execute(self, move_data, should_promote):
+        move = Move(*move_data)
+        from_piece_pre_move = self.test_board.get_piece(move.from_x, move.from_y)
+        move.execute(self.test_board)
+        from_piece_post_move = self.test_board.get_piece(move.from_x, move.from_y)
+        to_piece_post_move = self.test_board.get_piece(move.to_x, move.to_y)
+        self.assertEqual(from_piece_post_move, pieces.Blank)
+
+        if should_promote:
+            self.assertEqual(from_piece_pre_move, pieces.Pawn)
+            self.assertEqual(to_piece_post_move, pieces.Queen)
+        else:
+            self.assertEqual(from_piece_pre_move, to_piece_post_move)
