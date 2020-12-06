366671984def2c2cf3aceeb6d428c9d4fbf710b2
Andres Bonilla <andresdb91@gmail.com>
2020-12-05 22:14:00 -0300

    Add update moves test for black and white pieces

--- test/test_board.py
+++ test/test_board.py
@@ -53,3 +53,50 @@ class TestBoard(unittest.TestCase):
     def test_to_char_array(self):
         expected = list(self.input_board)
         self.assertEqual(self.board.to_char_array(), expected)
+
+    @parameterized.expand([
+        (
+            'r h b q k       '
+            '                '
+            'ppp pppp        '
+            '   p            '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            'PPPPPPPP        '
+            '                '
+            'R H B Q K       ',
+            'black',
+            (3, 2, 3, 3),
+        ),
+        (
+            'r h b q k       '
+            '                '
+            'pppppppp        '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '                '
+            '   P            '
+            'PPP PPPP        '
+            '                '
+            'R H B Q K       ',
+            'white',
+            (3, 13, 3, 12),
+        ),
+    ])
+    def test_update_board(self, modified_board, color, expected_move):
+        move = self.board.update(modified_board, color)
+        move_coords = move.to_coords()
+        self.assertEqual(move_coords, expected_move)
