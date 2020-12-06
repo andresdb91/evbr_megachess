f1bd6852a82236be071e3d382dbb4c88cf5a4910
Andres Bonilla <andresdb91@gmail.com>
2020-12-06 00:04:51 -0300

    Improve strategy factory structure

--- game/strategies_fact.py
+++ game/strategies_fact.py
@@ -2,22 +2,21 @@ import game.strategies
 
 
 class AIStrategyFactory:
-    default_strategy: game.strategies.BaseStrategy = game.strategies.RandomLegal
+    default_strategy: str = 'random_legal'
 
     @staticmethod
     def get_strategy(name: str) -> game.strategies.BaseStrategy:
+        strategies = {
+            'random_legal': game.strategies.RandomLegal,
+            'maximum_points': game.strategies.MaximumPointMove,
+            'maximum_weight': game.strategies.MaximumWeight,
+            'pawns_and_queens': game.strategies.OnlyPawnsAndQueensByWeight,
+            '2_move_weight': game.strategies.TwoMoveWeighting,
+            'multi_move_weight': game.strategies.MultiMoveWeight,
+        }
+
+        if name not in strategies.keys():
+            name = AIStrategyFactory.default_strategy
         print(f'Using strategy: {name}')
-        if name == 'random_legal':
-            return game.strategies.RandomLegal()
-        elif name == 'maximum_points':
-            return game.strategies.MaximumPointMove()
-        elif name == 'maximum_weight':
-            return game.strategies.MaximumWeight()
-        elif name == 'pawns_and_queens':
-            return game.strategies.OnlyPawnsAndQueensByWeight()
-        elif name == '2_move_weight':
-            return game.strategies.TwoMoveWeighting()
-        elif name == 'multi_move_weight':
-            return game.strategies.MultiMoveWeight()
-        else:
-            return AIStrategyFactory.default_strategy()
+
+        return strategies[name]()
